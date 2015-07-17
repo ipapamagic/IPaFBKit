5d31d9516442d902f44e025b8dc1e7ff434fe4a5
IPa Chen <ipapamagic@gmail.com>
2015-07-17 17:56:44 +0800

    goodbye Objective C

--- IPaFBKit/IPaFBKit.h
+++ IPaFBKit/IPaFBKit.h
@@ -2,29 +2,18 @@
 //  IPaFBKit.h
 //  IPaFBKit
 //
-//  Created by IPa Chen on 2014/11/16.
-//  Copyright 2014年 A Magic Studio. All rights reserved.
+//  Created by IPa Chen on 2015/7/10.
+//  Copyright (c) 2015年 A Magic Studio. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
-#import <FBSDKCoreKit/FBSDKCoreKit.h>
-#import <FBSDKShareKit/FBSDKShareKit.h>
-#import <FBSDKLoginKit/FBSDKLoginKit.h>
-@import UIKit;
-@interface IPaFBKit : NSObject {
-    
-}
-+ (id)sharedInstance;
-- (BOOL)isFBLogin;
-- (void)logoutFB;
+#import <UIKit/UIKit.h>
 
-- (void)loginFBWithPublishPermissions:(NSArray*)permissions defaultAudience:(FBSDKDefaultAudience)defaultAudience callback:(void (^)(BOOL))callback;
-- (void)loginFBWithReadPermissions:(NSArray*)permissions callback:(void (^)(BOOL))callback;
+//! Project version number for IPaFBKit.
+FOUNDATION_EXPORT double IPaFBKitVersionNumber;
 
-//- (void)shareLink:(NSString*)link name:(NSString*)name caption:(NSString*)caption description:(NSString*)description picture:(NSString*)picture callback:(void (^)(BOOL))callback;
-- (void)sharePhoto:(UIImage*)image params:(NSDictionary*)params callback:(void (^)(NSString*))callbak;
-- (void)postOnFeedWithName:(NSString*)name caption:(NSString*)caption description:(NSString*)description picture:(NSString*)picture link:(NSString*)link callback:(void (^)(id, NSError *))callback;
-//get facebook user image link
-- (NSString*)imageLinkWithFBID:(NSString*)fbID;
-- (NSString*)imageLinkWithFBID:(NSString *)fbID type:(NSString*)type;
-@end
+//! Project version string for IPaFBKit.
+FOUNDATION_EXPORT const unsigned char IPaFBKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IPaFBKit/PublicHeader.h>
+
+
---
+++ IPaFBKit/IPaFBKit.swift
@@ -0,0 +1,74 @@
+//
+//  IPaFBKit.swift
+//  IPaFBKit
+//
+//  Created by IPa Chen on 2015/7/10.
+//  Copyright (c) 2015年 A Magic Studio. All rights reserved.
+//
+
+import Foundation
+import FBSDKCoreKit
+import FBSDKLoginKit
+import FBSDKShareKit
+@objc public class IPaFBKit {
+    static public func imageLink(fbID:String,type:String) -> String
+    {
+        return "http://graph.facebook.com/\(fbID)/picture?type=\(type)"
+    }
+    static public func imageLink(fbID:String) -> String
+    {
+        return imageLink(fbID,type:"normal")
+    }
+
+    static public func isFBLogin() -> Bool {
+        return FBSDKAccessToken.currentAccessToken() != nil
+    }
+
+    //MARK: Share
+    static public func sharePhoto(image:UIImage, params:[String:AnyObject], complete:((String?) -> ()))
+    {
+        var mParams = params
+        mParams["source"] = UIImageJPEGRepresentation(image, 1);
+        let request = FBSDKGraphRequest(graphPath: "/me/photos", parameters: mParams, HTTPMethod: "POST")
+        request.startWithCompletionHandler({
+            connection,result,error in
+            if error != nil {
+                complete(nil)
+            }
+            else {
+                if let result = result as? [String:AnyObject] {
+                    if let id = result["id"] as? String {
+                        complete(id)
+                        return
+                    }
+                }
+                complete(nil)
+            }
+        })
+    }
+    static public func postOnFeed(name:String?, caption:String?, description:String? , picture:String?, link:String?, complete:((AnyObject, NSError) -> ()))
+    {
+        var mParams = [String:AnyObject]()
+        if let name = name {
+            mParams["name"] = name
+        }
+        if let caption = caption {
+            mParams["caption"] = caption
+        }
+        if let description = description {
+            mParams["description"] = description
+        }
+        if let picture = picture {
+            mParams["picture"] = picture
+        }
+        if let link = link {
+            mParams["link"] = link
+        }
+        let request = FBSDKGraphRequest(graphPath: "/me/feed", parameters: mParams, HTTPMethod: "POST")
+        request.startWithCompletionHandler({
+            connection,result,error in
+            complete(result,error)
+        })
+
+    }
+}
