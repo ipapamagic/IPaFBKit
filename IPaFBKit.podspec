b8c07ac0a959f805d72dda38c1cd5baf77fc2265
IPaPa <ipapamagic@gmail.com>
2022-08-11 01:21:52 +0800

    support swift package manager

--- IPaFBKit.podspec
+++ IPaFBKit.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'IPaFBKit'
-  s.version          = '2.3'
+  s.version          = '2.4.0'
   s.summary          = 'IPaFBKit is a tool kit for using Facebook SDK'
   s.swift_version    = '5.0'
 # This description is used to generate tags and improve search results.
---
+++ Tests/IPaFBKitTests/IPaFBKitTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import IPaFBKit
+
+final class IPaFBKitTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+//        XCTAssertEqual(IPaFBKit().text, "Hello, World!")
+    }
+}
